9949d679c95617017dbb58ef6a2a4e4499650893
XufengCh <xfch293@gmail.com>
2019-10-28 21:01:10 +0800

    重构了项目的代码结构 新增主页地址127.0.0.1

--- HouseOfFashion/HouseOfFashion/urls.py
+++ HouseOfFashion/HouseOfFashion/urls.py
@@ -19,5 +19,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('change.urls')),
     path('change/', include('change.urls')),
 ]
