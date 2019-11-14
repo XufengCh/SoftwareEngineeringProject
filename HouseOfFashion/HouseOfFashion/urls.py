67d94f9d1318e24a11bef11a42ba0664e6866c54
KaLuLas <499485532@qq.com>
2019-11-15 00:43:22 +0800

    update

--- HouseOfFashion/HouseOfFashion/urls.py
+++ HouseOfFashion/HouseOfFashion/urls.py
@@ -21,4 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('change.urls')),
     path('change/', include('change.urls')),
+    path('users/', include('users.urls')),
+    path('users/', include('django.contrib.auth.urls')),
 ]
