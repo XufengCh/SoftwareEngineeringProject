97f8a49d32d0a1548eef12107f7ff63f8e574949
XufengCh <xfch293@gmail.com>
2019-11-14 15:51:33 +0800

    try to finish REGISTER

--- HouseOfFashion/users/views.py
+++ HouseOfFashion/users/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import RegisterForm
 
 # Create your views here.
+def register(request):
+    if request.method == 'POST':
+        request.POST['email'] = request.POST['username']
+        form = RegisterForm(request.POST)
