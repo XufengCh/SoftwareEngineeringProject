0e29ef59d4fc490852480cb36b8ee57954a5dfcd
XufengCh <xfch293@gmail.com>
2019-12-10 23:11:49 +0800

    rebuild database

--- HouseOfFashion/change/models.py
+++ HouseOfFashion/change/models.py
@@ -1,10 +1,12 @@
 from django.db import models
-from images.models import Clothe, Body
+from images.models import ClotheImage, BodyImage
 
 
 # Create your models here.
 class CompositeImage(models.Model):
-    body = models.ForeignKey(to=Body, on_delete=models.CASCADE)
-    clothe = models.ForeignKey(to=Clothe, on_delete=models.CASCADE)
+    create_time = models.DateTimeField(auto_now_add=True)
+    body_image = models.ForeignKey(to=BodyImage, on_delete=models.CASCADE)
+    clothe_image = models.ForeignKey(to=ClotheImage, on_delete=models.CASCADE)
 
-    change_result = models.FileField(upload_to='composite-image/%Y/%m/%d')
+    composite_image = models.FileField(upload_to='composite-image/%Y/%m/%d')
+    score = models.CharField(max_length=10)
